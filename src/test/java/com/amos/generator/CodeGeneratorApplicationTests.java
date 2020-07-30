eb1895341807b9ecb84f864d25742c31419aa9b9
shali001 <771819799@qq.com>
2020-07-30 11:18:04 +0800

    init

---
+++ src/test/java/com/amos/generator/CodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amos.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
